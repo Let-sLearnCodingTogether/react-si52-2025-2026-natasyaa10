7f6764a0b2649fcdce0cef54e624a48958a0972c
natasyaa10 <natasyapriscill10@gmail.com>
2025-11-15 18:34:35 +0700

    Pertemuan tgl 15 nov 2025

---
+++ src/utils/apiClient.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const ApiClient = axios.create({
+    baseURL : "http://localhost:3000/api",
+    headers : {
+        "Accept" : "application/json"
+    }
+}
+
+)
+
+export default ApiClient;
