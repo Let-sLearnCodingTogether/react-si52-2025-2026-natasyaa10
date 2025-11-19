d343f804d51041e95cb547479a7a77643d1e59bb
natasyaa10 <natasyapriscill10@gmail.com>
2025-11-19 20:25:34 +0700

    Pertemuan tgl 19 nov 2025

--- src/pages/movies/Movies.tsx
+++ src/pages/movies/Movies.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useState } from "react"
 import { NavLink } from "react-router"
 import ApiClient from "../../utils/apiClient"
+import { Button, Table } from "react-bootstrap"
 
 interface Movie {
     _id : string,
@@ -27,12 +28,43 @@ function Movies() {
         fetchMovies()
     }, [fetchMovies])
 
+    const handleDelete = async (movieId : String) => {
+        const response = await ApiClient.delete(`/movie/${movieId}`)
+
+        if (response.status == 200) {
+            fetchMovies()
+        }
+    }
+
     return <div className="container mx-auto">
         <div className="d-flex justify-content-between mb-3"> 
             <h2>Movie Page</h2>
             <NavLink to="/add-movie" className="btn btn-primary"> Add Movie </NavLink>
         </div>
-    </div>
+            <Table striped bordered hover>
+                <thead>
+                    <th>No</th>
+                    <th>Judul</th>
+                    <th>Tahun Rilis</th>
+                    <th>Sutradara</th>
+                    <th>Aksi</th>
+                </thead>
+                <tbody>
+                    {
+                        movies.length > 0 && movies.map((movies, index) => {
+                            return <tr key = {movies._id}>
+                                <td>{index + 1}</td>
+                                <td>{movies.judul}</td>
+                                <td>{movies.tahunRilis}</td>
+                                <td>{movies.sutradara}</td>
+                                <td><Button variant="danger" onClick={() => handleDelete(movies._id)}>Delete</Button></td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </Table>
+        </div>
+    
 }
 
 export default Movies
